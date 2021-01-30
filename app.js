ba558345e07d3d3c90a100bf7872b249e5367e72
Shashwat Navinkumar <000790494@mohawkcollege.ca>
2021-01-29 19:32:48 -0500

    changed port code structure in app js

--- app.js
+++ app.js
@@ -172,7 +172,12 @@ app.post("/delete", function(req,res){
      }
 });
 
-app.listen(process.env.PORT || 3000, function(){
+let port = process.env.PORT;
+if(port == null || port == "") {
+    port = 3000;
+}
+
+app.listen(port, function(){
     console.log("Server has started successfully")
 });
 
